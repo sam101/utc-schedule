94b9517d8c0954474ac18eafd9e816eba9b78059
Samuel Lepetit <lepetit.samuel@gmail.com>
2014-05-26 10:38:06 +0200

    Add a stupid main to ask people for their username/password

--- src/com/slepetit/utc/schedule/Schedule.java
+++ src/com/slepetit/utc/schedule/Schedule.java
@@ -3,6 +3,7 @@ package com.slepetit.utc.schedule;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Schedule {
 	private static final String DATA_PATH = "data/edt";
@@ -11,7 +12,15 @@ public class Schedule {
 		
 		Files.createDirectories(Paths.get("data/edt"));
 		
-		new ScheduleNetworkClient("login", "passwrd", DATA_PATH);
+		Scanner s = new Scanner(System.in);
+		System.out.println("Login ?");
+		String login = s.next();
+		System.out.println("Password ?");
+		String password = s.next();
+		
+		s.close();
+		
+		new ScheduleNetworkClient(login, password, DATA_PATH);
 		
 		ScheduleParser parser = new ScheduleParser(DATA_PATH);
 		parser.parseAllUsers();
